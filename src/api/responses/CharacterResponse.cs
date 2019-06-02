c3e27e03a55a26fc89f78a7f60e0d05b8611e37d
Mathieu Morrissette <mathieumorrissette@gmail.com>
2019-06-02 18:57:30 -0400

    initial commit

---
+++ src/api/responses/CharacterResponse.cs
@@ -0,0 +1,31 @@
+﻿
+using HttpServer.websites.exalted.managers;
+using HttpServer.websites.exalted.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer.websites.exalted.api.responses
+{
+    public class CharacterResponse
+    {
+        public int id { get; set; }
+
+        public int owner { get; set; }
+
+        public string data { get; set; }
+
+        public static CharacterResponse FromCharacter(Character character)
+        {
+            CharacterResponse response = new CharacterResponse();
+
+            response.id = character.Id;
+            response.owner = character.UserId;
+            response.data = character.Data;
+
+            return response;
+        }
+    }
+}
